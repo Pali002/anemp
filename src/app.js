0187156bc91f25c8192d9396d840439b4de60aea
Zentai Pál <zentaipali@gmail.com>
2022-09-21 11:23:44 +0200

    torles, hozzaadas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const dowButton = document.querySelector("#dowButton");
+const addButton = document.querySelector("#addButton");
 const empTable = document.querySelector("#empTable");
+const empName = document.querySelector("#name");
+
+const host = 'http://localhost:3000';
 
 dowButton.addEventListener('click', () => {
-    console.log('Letoltes');
-    
-var url = 'http://localhost:3000/employees';
+    let endpoint = 'employees';
+    let url = host + '/' + endpoint;
 
     fetch(url)
     .then(response => response.json())
@@ -19,13 +22,22 @@ var url = 'http://localhost:3000/employees';
 });
 
 function renderTable(employees) {
+    empTable.textContent = '';
     //console.log(employees[1].name);
     employees.forEach(employee => {
         let tr = document.createElement('tr');
         let tdId = document.createElement('td');
         let tdName = document.createElement('td');
+        let tdDel = document.createElement('td');
+        let delBtn = document.createElement('button');
+        delBtn.textContent = 'Törlés';
+        delBtn.addEventListener('click', () => {
+            deleteEmployee(employee.id);
+        });
         tr.appendChild(tdId);
         tr.appendChild(tdName);
+        tr.appendChild(tdDel);
+        tdDel.appendChild(delBtn);
         empTable.appendChild(tr);
     
         tdId.textContent = employee.id;
@@ -35,7 +47,34 @@ function renderTable(employees) {
 
 };
 
-// //Elso dolgozo neve
-// fetch(url)
-// .then(response => response.json())
-// .then(result => {console.log(result[2].name)});
+addButton.addEventListener('click', () => {
+    let endpoint = 'employees';
+    let url = host + '/' + endpoint;
+    let employee = {
+        name: empName.value
+    };
+    fetch(url, {
+        method: 'post',
+        body: JSON.stringify(employee),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+    });
+});
+
+function deleteEmployee(id) {
+    console.log(id);
+    let endpoint = 'employees';
+    let url = host + '/' + endpoint + '/' +id;
+    fetch(url, {
+        method: 'delete'
+    })
+    .then(response => response.json())
+    .then(result =>{
+        console.log(result);
+    });
+};
