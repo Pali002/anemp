4b19eb6f58455534206b342850ee266539977bdc
Zentai Pál <zentaipali@gmail.com>
2022-09-21 09:15:25 +0200

    Dolgozok lekerese kesz

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-var url = 'http://localhost:3000/employees';
-fetch(url)
-.then(result => result.json())
-.then(data => {
-    console.log(data);
+const dowButton = document.querySelector("#dowButton");
+
+dowButton.addEventListener('click', () => {
+    console.log('mukszik');
 });
+
+var url = 'http://localhost:3000/employees';
+
+fetch(url)
+.then(response => response.json())
+.then(result => {console.log(result)})
+.catch(error => {
+    console.log('Hiba! Lekerdezés sikertelen');
+    console.log(error);
+});
+
+
+// //Elso dolgozo neve
+// fetch(url)
+// .then(response => response.json())
+// .then(result => {console.log(result[2].name)});
